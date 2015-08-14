cebe9aa0ae00a2ec73fc5be32bd9d1b075e57931
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2015-08-14 10:37:11 -0600

    match all urls

--- index.rb
+++ index.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 TEMPORAL = 302
 
-get '/' do
+get '/*' do
   return 'REDIRECT_URL is not set' if ENV['REDIRECT_URL'].nil?
   http_code = ENV['REDIRECT_CODE'] || TEMPORAL
   redirect ENV['REDIRECT_URL'], http_code
